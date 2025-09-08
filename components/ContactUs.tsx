cfaf94d2dd0518112f829a15a6a1d29b01206ad9
manavlade <manavlade14690@gmail.com>
2025-09-08 13:11:23 +0530

    chnages

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
